ff5f8e4f0ebb00eb197e342e436275f8a7e2ca33
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-10-25 15:25:47 -0400

    fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import styled from 'styled-components'
+import Reviews from '../components/Reviews'
 
 const About = () => {
   return (
@@ -9,8 +11,18 @@ const About = () => {
         <meta name='description' content='Welcome to our About Page' />
         <link rel='canonical' href='/about' />
       </Helmet>
+      <Wrapper>
+        <div className='box-1'>
+          <h1>hello</h1>
+        </div>
+        <Reviews />
+      </Wrapper>
     </>
   )
 }
-
+const Wrapper = styled.div`
+  .box-1 {
+    height: 100vh;
+  }
+`
 export default About
